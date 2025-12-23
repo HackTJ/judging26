77bb4421f1dc9df8392ab4ff8bbec70e6dbbfc40
Shreyas Jain <shrys.jain@gmail.com>
2025-12-23 15:08:40 -0500

    register user model

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,15 @@ class User(AbstractUser):
 
   def __str__(self):
     return f"{self.username} ({self.role})"
+  
+  @property
+  def is_admin(self):
+    return self.role == self.Role.ADMIN
+
+  @property
+  def is_judge(self):
+    return self.role == self.Role.JUDGE
+
+  @property
+  def is_team(self):
+    return self.role == self.Role.TEAM
