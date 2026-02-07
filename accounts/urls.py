476db5a9cd6066376f73e56bcb398eecd1bf8e4a
Jacob Percy <145606204+JacobPercy@users.noreply.github.com>
2026-02-06 21:07:46 -0500

    fixed it up

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-  path("", views.dashboard, name="home"),
+  path("", RedirectView.as_view(url="/dashboard/", permanent=False), name="home"),
   path("dashboard/", views.dashboard, name="dashboard"),
   path("presentations/<int:project_id>/", views.presentation_viewer, name="presentation_viewer"),
 ]
